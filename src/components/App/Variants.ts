594c25aafa46f0bd9bd7675804a99305a7ead1a1
Chukwu Stanley <stanleychukwu17@yahoo.com>
2022-12-10 06:18:01 +0100

    change the intro animation

--- src/components/App/Variants.ts
+++ src/components/App/Variants.ts
@@ -2,10 +2,10 @@ import { Variant } from "framer-motion";
 
 export const cardVariant: {initial?: Variant, animate?: Variant} = {
     initial: (custom) => ({
-        y: -60, opacity:0,
+        x: -60, opacity:0,
     }),
     animate: (custom) => ({
-        y:0, opacity:1,
+        x:0, opacity:1,
         transition: {
             duration: 1,
             delay: custom * 0.2,
